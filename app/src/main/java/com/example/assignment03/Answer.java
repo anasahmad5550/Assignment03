6928eec7cf24b460ac80d1dadfbc495fb344e94e
Anas Ahmad <ahmadanas4490@gmail.com>
2020-12-21 11:47:52 +0500

    Initial commit

---
+++ app/src/main/java/com/example/assignment03/Answer.java
@@ -0,0 +1,42 @@
+package com.example.assignment03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Answer extends AppCompatActivity {
+    TextView score;
+    Button btnRestart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+
+        score = (TextView)findViewById(R.id.score);
+        btnRestart = (Button) findViewById(R.id.btnRestart);
+
+
+        StringBuffer stringbuff = new StringBuffer();
+        stringbuff.append("Your Score is : " + Questions.correct + "/10");
+
+        score.setText(stringbuff);
+
+        Questions.correct=0;
+
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/assignment03/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.assignment03;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+    ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView=findViewById(R.id.nav_view);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId())
+                {
+                    case R.id.nav_calculator :
+
+                        Intent intent = new Intent(MainActivity.this,calculator.class);
+                        startActivity(intent);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    case R.id.Nav_quiz :
+                        Intent intent1 = new Intent(MainActivity.this, quiz.class);
+                        startActivity(intent1);
+                        //drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+
+                }
+
+                return true;
+            }
+        });
+
+    }
+}
